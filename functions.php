e30232b34469013f6297259533060158b998e5aa
Nate Bailey <nate@inthepin.es>
2014-08-31 14:58:15 -0400

    Filter out tags without published items

--- functions.php
+++ functions.php
@@ -1,14 +1,17 @@
-/*
-  Returns an array of all valid page tags
-  @TODO: Filter out tags without published pages
-**/
+<?php
+
+//  Returns an array of all valid page tags
 function get_page_tags() {
   $tag_ext = Extend::where('key', '=', 'tags')->where('data_type', '=', 'page')->get();
   $tag_id = $tag_ext[0]->id;
 
   $tags = array();
   $index = 0;
-  foreach(Query::table('page_meta')->where('extend', '=', $tag_id)->get() as $meta) {
+  foreach(Query::table('page_meta')
+    ->left_join('pages', 'pages.id', '=', 'page_meta.page')
+    ->where('pages.status', '=', 'published')
+    ->where('extend', '=', $tag_id)
+    ->get() as $meta) {
     $page_meta = json_decode($meta->data);
     foreach(explode(", ", $page_meta->text) as $tag_text) {
       $tags[$index] = $tag_text;
@@ -19,9 +22,7 @@ function get_page_tags() {
   return array_unique($tags);
 }
 
-/*
-  Finds pages with the specified tag and passes an array of their ids
-**/
+//  Finds pages with the specified tag and passes an array of their ids
 function get_pages_with_tag($tag='') {
   $tag_ext = Extend::where('key', '=', 'tags')->get();
   $tag_id = $tag_ext[0]->id;
@@ -38,17 +39,18 @@ function get_pages_with_tag($tag='') {
   return array_unique($pages);
 }
 
-/*
-  Returns an array of all valid post tags
-  @TODO: Filter out tags without published posts
-**/
+//  Returns an array of all valid post tags
 function get_post_tags() {
   $tag_ext = Extend::where('key', '=', 'tags')->where('data_type', '=', 'post')->get();
   $tag_id = $tag_ext[0]->id;
 
   $tags = array();
   $index = 0;
-  foreach(Query::table('post_meta')->where('extend', '=', $tag_id)->get() as $meta) {
+  foreach(Query::table('post_meta')
+    ->left_join('posts', 'posts.id', '=', 'post_meta.post')
+    ->where('posts.status', '=', 'published')
+    ->where('extend', '=', $tag_id)
+    ->get() as $meta) {
     $post_meta = json_decode($meta->data);
     foreach(explode(", ", $post_meta->text) as $tag_text) {
       $tags[$index] = $tag_text;
@@ -59,9 +61,7 @@ function get_post_tags() {
   return array_unique($tags);
 }
 
-/*
-  Finds posts with the specified tag and passes an array of their ids
-**/
+//  Finds posts with the specified tag and passes an array of their ids
 function get_posts_with_tag($tag='') {
   $tag_ext = Extend::where('key', '=', 'tags')->get();
   $tag_id = $tag_ext[0]->id;
